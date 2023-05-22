9342def102891da545553863b14fe95f7a7ee4a0
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-22 15:18:26 +0200

    add construct

--- models/movie.php
+++ models/movie.php
@@ -4,6 +4,11 @@
     public $title;
     public $director;
     public $genre;
-  }
 
+    public function __construct($_title, $_director, $_genre) {
+      $this->title = $_title;
+      $this->director = $_director;
+      $this->genre = $_genre;
+    }
+  }
 ?>
